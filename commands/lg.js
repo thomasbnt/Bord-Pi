13f05c1c209fee015bb3317667f3878d7e066535
Thomas Bnt <thomasbnt@protonmail.com>
2019-05-04 03:35:09 +0200

    Modification des rôles d'accès

--- commands/lg.js
+++ commands/lg.js
@@ -1,13 +1,18 @@
 const Discord = require('discord.js')
 exports.run = async (bot, WebhookPrivate, WebhookPublic, msg) => {
-    if (msg.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")) { msg.delete(msg.author).catch(e => console.error("ℹ Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur.")) };
+    if (msg.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")) { msg.delete(msg.author).catch(e => console.log(bot.ls.info, "Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur.")) }
     if (msg.member.roles.has(bot.config.roles.id.LoupGarou)) {
         msg.member.removeRole(bot.config.roles.id.LoupGarou).catch(bot.ls.error, console.error)
         msg.channel.send("Vous avez quitté le village.")
             .then(m => { setTimeout(() => { m.delete() }, 20000) })
 
         console.log(bot.ls.info, bot.config.prefix + "lg " + " de " + msg.author.tag + " (" + msg.author.id + ")")
-        WebhookPrivate.send("**" + bot.config.prefix + "lg** - De ``" + msg.author.username + "#" + msg.author.discriminator + "``, ID : ``" + msg.author.id + "``")
+        const LogLGIfPrivateEmbed = new Discord.RichEmbed()
+        WebhookPrivate.send(LogLGIfPrivateEmbed
+            .setColor(bot.config.roles.color.LoupGarou)
+            .setDescription("Rôle **" + bot.config.roles.name.LoupGarou + "** supprimé pour " + msg.author)
+            .setFooter("ID : " + msg.author.id, msg.author.avatarURL)
+        )
         const LogLGIfEmbed = new Discord.RichEmbed()
         WebhookPublic.send(LogLGIfEmbed
             .setColor(bot.config.roles.color.LoupGarou)
@@ -21,7 +26,12 @@ exports.run = async (bot, WebhookPrivate, WebhookPublic, msg) => {
             .then(m => { setTimeout(() => { m.delete() }, 20000) })
 
         console.log(bot.ls.info, bot.config.prefix + "lg " + " de " + msg.author.tag + " (" + msg.author.id + ")")
-        WebhookPrivate.send("**" + bot.config.prefix + "lg** - De ``" + msg.author.username + "#" + msg.author.discriminator + "``, ID : ``" + msg.author.id + "``")
+        const LogLGElsePrivateEmbed = new Discord.RichEmbed()
+        WebhookPrivate.send(LogLGElsePrivateEmbed
+            .setColor(bot.config.roles.color.LoupGarou)
+            .setDescription("Rôle **" + bot.config.roles.name.LoupGarou + "** ajouté pour " + msg.author)
+            .setFooter("ID : " + msg.author.id, msg.author.avatarURL)
+        )
         const LogLGElseEmbed = new Discord.RichEmbed()
         WebhookPublic.send(LogLGElseEmbed
             .setColor(bot.config.roles.color.LoupGarou)
--- commands/thegate.js
+++ commands/thegate.js
@@ -1,13 +1,18 @@
 const Discord = require('discord.js')
 exports.run = async (bot, WebhookPrivate, WebhookPublic, msg) => {
-    if (msg.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")) { msg.delete(msg.author).catch(e => console.error("ℹ Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur.")) };
+    if (msg.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")) { msg.delete(msg.author).catch(e => console.log(bot.ls.info, "Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur.")) };
     if (msg.member.roles.has(bot.config.roles.id.thegate)) {
         msg.member.removeRole(bot.config.roles.id.thegate).catch(bot.ls.error, console.error)
         msg.channel.send("Vous n'avez plus accès à la ** catégorie de The Gate **.")
             .then(m => { setTimeout(() => { m.delete() }, 20000) })
 
         console.log(bot.ls.info, bot.config.prefix + "thegate " + " de " + msg.author.tag + " (" + msg.author.id + ")")
-        WebhookPrivate.send("**" + bot.config.prefix + "thegate** - De ``" + msg.author.username + "#" + msg.author.discriminator + "``, ID : ``" + msg.author.id + "``")
+        const LogTheGateIfPrivateEmbed = new Discord.RichEmbed()
+        WebhookPrivate.send(LogTheGateIfPrivateEmbed
+            .setColor(bot.config.roles.color.thegate)
+            .setDescription("Rôle **" + bot.config.roles.name.thegate + "** supprimé pour " + msg.author)
+            .setFooter("ID : " + msg.author.id, msg.author.avatarURL)
+        )
         const LogTheGateIfEmbed = new Discord.RichEmbed()
         WebhookPublic.send(LogTheGateIfEmbed
             .setColor(bot.config.roles.color.thegate)
@@ -21,7 +26,12 @@ exports.run = async (bot, WebhookPrivate, WebhookPublic, msg) => {
             .then(m => { setTimeout(() => { m.delete() }, 20000) })
 
         console.log(bot.ls.info, bot.config.prefix + "thegate " + " de " + msg.author.tag + " (" + msg.author.id + ")")
-        WebhookPrivate.send("**" + bot.config.prefix + "thegate** - De ``" + msg.author.username + "#" + msg.author.discriminator + "``, ID : ``" + msg.author.id + "``")
+        const LogTheGateElsePrivateEmbed = new Discord.RichEmbed()
+        WebhookPrivate.send(LogTheGateElsePrivateEmbed
+            .setColor(bot.config.roles.color.thegate)
+            .setDescription("Rôle **" + bot.config.roles.name.thegate + "** ajouté pour " + msg.author)
+            .setFooter("ID : " + msg.author.id, msg.author.avatarURL)
+        )
         const LogTheGateElseEmbed = new Discord.RichEmbed()
         WebhookPublic.send(LogTheGateElseEmbed
             .setColor(bot.config.roles.color.thegate)
