622b7e701c4b6c043f24d60916cab6b9964def96
Thomas Bnt <thomasbnt@protonmail.com>
2019-10-01 22:04:57 +0200

    Update form.js

--- commands/Info/form.js
+++ commands/Info/form.js
@@ -6,7 +6,7 @@ exports.run = (bot, WebhookPrivate, WebhookPublic, msg) => {
 
     if (msg.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")) {
         msg.delete(msg.author).catch(e => console.log(bot.ls.info, "Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur."))
-    };
+    }
 
     msg.channel.send(
         new Discord.RichEmbed()
