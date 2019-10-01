c6bfe9ecb06a628bfe832435a922f886d6a30c64
Thomas Bnt <thomasbnt@protonmail.com>
2019-10-01 19:35:08 +0200

    Embed uptime

--- commands/Info/reddit.js
+++ commands/Info/reddit.js
@@ -8,7 +8,8 @@ exports.run = (bot, WebhookPrivate, WebhookPublic, msg) => {
   if (msg.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")) {
     msg.delete(msg.author).catch(e => console.log(bot.ls.info, "Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur."))
   }
-  msg.channel.send(Embed.setColor(bot.config.PrimaryColor)
+  msg.channel.send(Embed
+    .setColor(bot.config.PrimaryColor)
     .setDescription("Nous avons un **Subreddit** à notre disposition ! Partagez ce que vous voulez, de la musique, des films/séries, de la tech/dev, de l'histoire, des articles en tout genre... tout ce que vous voulez !\n\n[Cliquez ici pour voir le Reddit](https://www.reddit.com/r/LaHype)")
   )
 
