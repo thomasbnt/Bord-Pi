478b61daaf94946cf6e7ae672f4903f524a8be9b
Thomas Bnt <thomasbnt@protonmail.com>
2019-10-02 18:03:30 +0200

    app:console.log(bot.ls.success

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ module.exports = (bot, WebhookPrivate, WebhookPublic, msg, args) => {
   console.log(bot.ls.success,"Connecté en tant que " + bot.user.username)
   bot.updatePresence()
 
-  let Motd = ["ThisIsFlume", "Henry III", "👋", "🍣", "😎", "E Corpé", "Mee1 premier du nom", "Raspi, se sentir utile !", "💡 Je suis née sur un Raspberry Pi !", "Dernière génération en route...", "Lorem Ipsum", "Loogé et coffré.", "Est-tu un 0 ou un 1?", "Le violet c'est beau", "🌧 > 🌞"]
+  let Motd = ["ThisIsFlume", "Henry III", "👋", "🍣", "😎", "E Corpé", "Mee1 premier du nom", "Raspi, se sentir utile !", "💡 Je suis né(e) sur un Raspberry Pi !", "Dernière génération en route...", "Lorem Ipsum", "Loogé et coffré.", "Est-tu un 0 ou un 1?", "Le violet c'est beau", "🌧 > 🌞"]
   let ThisIsMotd = Motd[Math.floor(Math.random() * Motd.length)]
   
   let twitch = new Twitch(bot, bot.config.TwitchChannelID)
