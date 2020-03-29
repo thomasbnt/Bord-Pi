0bd9ece8687bbd136b8c0b127d51444bcbf10f2a
Thomas Bnt <thomasbnt@protonmail.com>
2020-03-29 22:27:58 +0200

    Modif lien du form

--- commands/Info/form.js
+++ commands/Info/form.js
@@ -8,9 +8,9 @@ exports.run = (bot, WebhookPublic, msg) => {
         msg.delete(msg.author).catch(e => console.log(bot.ls.info, "Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur."))
     }
 
-    msg.channel.send(new Discord.RichEmbed()
+    msg.channel.send(new Discord.MessageEmbed()
         .setColor(bot.config.PrimaryColor)
-        .setDescription("Vous voulez nous rejoindre en tant que membre de l'équipe **La Hype_** ? Ou tout simplement devenir Partenaire ? Remplissez [ce formulaire](https://thomasbnt.fr/form) et on vous répondra dès que possible !")
+        .setDescription("Vous voulez nous rejoindre en tant que membre de l'équipe **La Hype_** ? Ou tout simplement devenir Partenaire ? Remplissez [ce formulaire](https://docs.google.com/forms/d/e/1FAIpQLScHhR0wv9ck3IgzL3AXeNrxWymdHEpxDyFi-o_9QHZFDmEXeA/viewform) et on vous répondra dès que possible !")
     )
 
     console.log(bot.ls.info, bot.config.prefix + "form " + " de " + msg.author.tag + " (" + msg.author.id + ")")
