554cc32b07d8980af97c8fb5bc1ea8664920ac75
Thomas Bnt <thomasbnt@protonmail.com>
2022-02-15 00:20:15 +0100

    Event ready

--- events/ready.js
+++ events/ready.js
@@ -1,26 +1,27 @@
-const Discord = require('discord.js')
+module.exports = {
+    name: 'ready',
+    description: 'Bot is ready',
+    execute(client) {
+        console.log(`Connecté en tant que ${client.user.username}`)
+        client.user.setActivity(`/bord`, { type: "WATCHING" })
 
-module.exports = (bot, WebhookPublic, msg, args) => {
-  console.log(bot.ls.success,"Connecté en tant que " + bot.user.username)
+        // Discord réclame l'activité sans raison. Le setInterval est seulement pour mettre à jour l'activité.
+        function Activity() {
+            client.user.setActivity(`/bord`, {
+                type: "WATCHING"
+            })
+        }
 
-  bot.user.setActivity(`${bot.config.prefix}bord`, {
-    type: "WATCHING"
-  })
-  // Discord claim Activity for no reasons. The setInterval is only for update the Activity.
-  function Activity() {
-    bot.user.setActivity(`${bot.config.prefix}bord`, {
-      type: "WATCHING"
-    })
-  }
-  setInterval(Activity, 1200000)
+        setInterval(Activity, 1200000)
 
-  let Motd = ["ThisIsFlume", "Henry III", "👋", "🍣", "😎", "E Corpé", "Mee1 premier du nom", "Raspi, se sentir utile !", "💡 Je suis né(e) sur un Raspberry Pi !", "Dernière génération en route...", "Lorem Ipsum", "Loogé et coffré.", "Est-tu un 0 ou un 1?", "Le violet c'est beau", "🌧 > 🌞"]
-  let ThisIsMotd = Motd[Math.floor(Math.random() * Motd.length)]
+        /*let Motd = ["ThisIsFlume", "Henry III", "👋", "🍣", "😎", "E Corpé", "Mee1 premier du nom", "Raspi, se sentir utile !", "💡 Je suis né(e) sur un Raspberry Pi !", "Dernière génération en route...", "Lorem Ipsum", "Loogé et coffré.", "Est-tu un 0 ou un 1?", "Le violet c'est beau", "🌧 > 🌞"]
+        let ThisIsMotd = Motd[Math.floor(Math.random() * Motd.length)]
 
-  WebhookPublic.send(new Discord.MessageEmbed()
-    .setColor(bot.config.SuccessColor)
-    .setAuthor("— Démarrage du robot !", bot.user.displayAvatarURL())
-    .setFooter(ThisIsMotd)
-    .setTimestamp(new Date())
-  ).catch(e => console.error(e))
+        WebhookPublic.send(new Discord.MessageEmbed()
+            .setColor(bot.config.SuccessColor)
+            .setAuthor("— Démarrage du robot !", bot.user.displayAvatarURL())
+            .setFooter(ThisIsMotd)
+            .setTimestamp(new Date())
+        ).catch(e => console.error(e))*/
+    }
 }
