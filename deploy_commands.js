1d6df44c7a9c68a19447e47ec7c2213baa3d93de
Thomas Bnt <thomasbnt@protonmail.com>
2022-03-15 18:52:15 +0100

    Deploy App Commands seulement sur le serveur concerné

--- deploy_commands.js
+++ deploy_commands.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
 const {REST} = require('@discordjs/rest')
 const {Routes} = require('discord-api-types/v9')
-const {clientId, token} = require('./config.json')
+const {clientId, serverId, token} = require('./config.json')
+
+if (!clientId) {
+    return console.error('clientId doit être inséré dans le fichier config.json')
+}
+if (!serverId) {
+  return console.error('serverId doit être inséré dans le fichier config.json')
+}
 
 const commands = []
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
@@ -13,6 +20,7 @@ for (const file of commandFiles) {
 
 const rest = new REST({version: '9'}).setToken(token)
 
-rest.put(Routes.applicationCommands(clientId), {body: commands})
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error)
+rest.put(
+    Routes.applicationGuildCommands(clientId, serverId),
+    {body: commands}
+).then(() => console.log('Successfully registered application commands.')).catch(console.error)
