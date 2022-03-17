711139b89d111d30cb40debf68b37a8b69dca61d
Thomas Bnt <thomasbnt@protonmail.com>
2022-03-17 19:41:57 +0100

    Ajout logs dans les events Member[Remove/Add]

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,41 +1,14 @@
-const Discord = require('discord.js')
-const moment = require('moment')
-moment.locale('FR')
 const config = require('../config.json')
+const BordPiHelper = require("../modules/BordPiHelper")
 
 module.exports = {
     name: 'guildMemberAdd',
     description: 'Guild Member Add',
-    execute(member) {
-        const guild = member.guild
-        /*const ChannelGeneral = member.guild.channels.cache.find(x => x.id === config.IDWelcomeChannel).catch(e => console.log(e))
-
-        ChannelGeneral.send(new Discord.MessageEmbed()
-            .setColor(config.PrimaryColor)
-            .addField(
-                "🍃 Bienvenue à " + member.user.username + " — Fiche d'aide",
-                "On vous souhaite la bienvenue sur **" + guild.name + "** ! Lisez les <#399600870804684803> avant tout.",
-                true)
-            .setFooter({text: "Ce message va s'autodétruire dans une minute"})
-        ).then((msg) => {
-            setTimeout(() => {
-                if (msg.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) {
-                    msg.delete(msg.author).catch(e => console.log("Optionnel : Le robot n'a pas la permission de supprimer la commande faite par l'utilisateur."))
-                }
-            }, 60000)
-        })*/
-
-        console.log(`📥  — ${member.user.tag} (${member.user.id}) a rejoint ${guild.name}`)
-
-        /*WebhookPublic.send(new Discord.MessageEmbed()
-            .setColor(bot.config.BlackColor)
-            .setAuthor(`📥 — ${member.user.username} nous a rejoint`, member.user.avatarURL())
-            .addField("Création", moment(member.user.createdTimestamp).format('ll'), true)
-            .addField("Jour(s)", checkDays(member.user.createdAt), true)
-            .setDescription(`Concernant ${member.user}`)
-            .setThumbnail(member.user.displayAvatarURL())
-            .setFooter(`ID : ${member.user.id}`)
-            .setTimestamp(new Date())
-        ).catch(e => console.error(e))*/
+    execute(client, member) {
+        const g = member.guilds.cache.get(config.serverId)
+        const ChannelGeneral = g.channels.cache.find(x => x.id === config.IDWelcomeChannel)
+        ChannelGeneral.send(`Bienvenue ${client.username} sur le serveur ${g.name}`)
+        BordPiHelper.logsinoutserver(client, `rejoint`, config.colors.SuccessColor)
+        console.log(`📥  — ${client.username} (${client.id}) a rejoint ${g.name}`)
     }
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,17 +1,12 @@
+const config = require('../config.json')
+const BordPiHelper = require("../modules/BordPiHelper")
 
 module.exports = {
     name: 'guildMemberRemove',
     description: 'Guild Member Remove',
-    execute(member) {
-        const guild = member.guild
-
-        console.log(`📤  — ${member.user.tag} (${member.user.id}) a quitté ${guild.name}`)
-
-        /*  WebhookPublic.send(new Discord.MessageEmbed()
-              .setColor(bot.config.BlackColor)
-              .setAuthor(`📤 — ${member.user.username} nous a quitté`, member.user.avatarURL)
-              .setFooter(`ID : ${member.user.id}`)
-              .setTimestamp(new Date())
-          ).catch(e => console.error(e))*/
+    execute(client, member) {
+        const g = member.guilds.cache.get(config.serverId)
+        BordPiHelper.logsinoutserver(client, `quitté`, config.colors.DangerColor)
+        console.log(`📥  — ${client.username} (${client.id}) a rejoint ${g.name}`)
     }
 }
