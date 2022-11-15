32a9226270943268c56e8848993d6cb736ecdfb3
Thomas Bnt <thomasbnt@protonmail.com>
2022-11-15 17:39:50 +0100

    :art: d.js : ActivityType

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,17 @@
-const cron = require('node-cron'),
-  synchronizeSlashCommands = require('../modules/SyncCommands')
+const cron = require('node-cron')
+const synchronizeSlashCommands = require('../modules/SyncCommands')
+const { ActivityType } = require('discord.js')
 module.exports = {
   name: 'ready',
   description: 'Bot is ready',
   async execute(client) {
     console.log(`Connecté en tant que ${client.user.username}`)
-    client.user.setActivity(`/bord`, { type: 'WATCHING' })
+    client.user.setActivity(`/bord`, { type: ActivityType.Watching })
 
     // Discord nettoie l'activité sans raison. Le setInterval est seulement pour le mettre à jour.
     cron.schedule('0 */6 * * *', () => {
       client.user.setActivity(`/bord`, {
-        type: 'WATCHING'
+        type: ActivityType.Watching
       })
     })
 
