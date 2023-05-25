d69f8f68ca23b3c9ab040af5a29b0e426dd7d677
Thomas Bnt <thomasbnt@protonmail.com>
2023-05-25 20:20:30 +0200

    :poop: (guildMemberRemove): "a quitté" instead of "a rejoint"

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -11,6 +11,6 @@ module.exports = {
       `quitté`,
       config.colors.DangerColor
     )
-    console.log(`📥  — ${member.username} (${member.id}) a rejoint ${guild.name}`)
+    console.log(`📥  — ${member.username} (${member.id}) a quitté ${guild.name}`)
   }
 }
