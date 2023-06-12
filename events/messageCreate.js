e2fe7d2abbe56197cfad3ba4ecb23b7260595559
Thomas Bnt <thomasbnt@protonmail.com>
2023-06-12 18:51:16 +0200

    :zap: 'client' is defined but never used

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,10 +2,10 @@ const config = require('../config.json')
 const FilterLinks = require('../modules/FilterLinks.js')
 module.exports = {
   name: 'messageCreate',
-  execute(msg, client) {
+  execute(msg) {
     if (msg.author.bot) return
     if (msg.author.id === config.clientId) return
     if (msg.channel.recipient) return
-    FilterLinks(msg, client)
+    FilterLinks(msg)
   }
 }
